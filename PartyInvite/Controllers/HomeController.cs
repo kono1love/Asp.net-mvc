3ce385583d2712ff332cf2d137ae24f3a040a0ac
Vladislav <Vladislav@KONO1LOVE>
2016-10-13 17:27:48 +0300

    Iniciate solution ASP.NET MVC

---
+++ PartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvite.Models;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "GM" : "GAfternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    [HttpPost]
+    public ViewResult RspvForm(GuestResponse guestResponse)
+        {
+            //TODO: Email response to the party organizer
+            return View("Thank you", guestResponse);
+        }
+    }
+}
