9e25d1abe83427ff9cef365a59b47e38a7d86b72
Vladislav <Vladislav@KONO1LOVE>
2016-10-18 11:54:52 +0300

    Create project EssentialTools. Set up Ninject. Add classes and views

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private Product[] products =
+        {
+              new Product {Name = "Kayak", Category ="Watersports", Price = 275M },
+              new Product {Name = "Lifejacket", Category ="Watersports", Price = 48.95M },
+              new Product {Name = "Football ball", Category ="Football", Price = 19.50M },
+              new Product {Name = "Corner flag", Category ="Football", Price = 34.95M }
+        };
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            ShoppingCart cart = new ShoppingCart(calc)
+            {
+                Products = products
+            };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
---
+++ EssentialTools/Infastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Infastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver (IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type servicesType)
+        {
+            return kernel.GetAll(servicesType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
